b9e01b7cc26b647ff19b1f2ffdeff42143d6abc9
ChrisMattew <cristian.matteu@gmail.com>
2022-08-16 12:43:37 +0200

    tailwind initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <div className="container" />
+      <div className="container">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </div>
   )
 }
